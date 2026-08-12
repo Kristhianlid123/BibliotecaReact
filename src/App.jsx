03ad062d00ad990943e23a21680607972c662eca
Kristhianlid123 <lidsmg3@gmail.com>
2026-08-12 15:26:13 -0500

    Iniciarlizar proyecto biblioteca con React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+    return (
+        <div>
+            <h1>Sistema Biblioteca</h1>
+
+            <p>
+                Gestión de libros y préstamos
+            </p>
+        </div>
+    );
+
+}
+
+export default App;
